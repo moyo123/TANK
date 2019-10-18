a00ab1e7dc07c96306a2469cb375cb0be548ed94
moyo123 <yuki-t-17d@stu.kbc.ac.jp>
2019-10-18 12:59:22 +0900

    簡易AI追加

---
+++ TANK/Assets/YUKI/Script/EnemyShot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShot : MonoBehaviour
+{
+    public Transform turret;
+    public Transform muzzle;
+    public GameObject bulletPrefab;
+
+    private float attackInterval = 2f;
+    private float turretRotationSmooth = 2.8f;
+    private float lastAttackTime;
+
+    private Transform player;
+
+    private void Start()
+    {
+        // 始めにプレイヤーの位置を取得できるようにする
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        // 砲台をプレイヤーの方向に向ける
+        Quaternion targetRotation = Quaternion.LookRotation(player.position - turret.position);
+        turret.rotation = Quaternion.Slerp(turret.rotation, targetRotation, Time.deltaTime * turretRotationSmooth);
+
+        // 一定間隔で弾丸を発射する
+        if (Time.time > lastAttackTime + attackInterval)
+        {
+            Instantiate(bulletPrefab, muzzle.position, muzzle.rotation);
+            lastAttackTime = Time.time;
+        }
+    }
+}
---
+++ TANK/Assets/YUKI/Script/ShellMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellMove : MonoBehaviour {
+
+    float Speed = 20f;
+    Rigidbody ShellRb;
+
+	// Use this for initialization
+	void Start () {
+        ShellRb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ShellRb.AddForce(transform.forward * Speed);
+	}
+}
