c2cdb5d697e9dfffcee187ec8d4c0f74f732240c
umitora7240 <kaito-s-17d@stu.kbc.ac.jp>
2019-11-13 10:48:28 +0900

    敵とかがプレイヤーにぶつかるとプレイヤーが勝手に動いていた現象を修正、AIを作る用にテストのシーンを追加

---
+++ TANK/Assets/KAITO/Script/Test_Navigation_kaito.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+public class Test_Navigation_kaito : MonoBehaviour {
+
+    [SerializeField]
+    GameObject target;
+
+    NavMeshAgent myNavMeshAgent;
+
+
+	// Use this for initialization
+	void Start () {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+        myNavMeshAgent.SetDestination(target.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            myNavMeshAgent.SetDestination(target.transform.position);
+        }
+		
+	}
+}
