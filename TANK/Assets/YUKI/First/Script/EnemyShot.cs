6a625cd0ade8b26d9ff350080dba4529bc755870
moyo123 <yuki-t-17d@stu.kbc.ac.jp>
2019-11-13 12:11:28 +0900

    最新にアップデート(おなかすいた)

--- TANK/Assets/YUKI/First/Script/EnemyShot.cs
+++ TANK/Assets/YUKI/First/Script/EnemyShot.cs
@@ -8,8 +8,10 @@ public class EnemyShot : MonoBehaviour
     public Transform muzzle;
     public GameObject bulletPrefab;
 
+    bool ShotFlg = false;
+
     private float attackInterval = 2f;
-    private float turretRotationSmooth = 2.8f;
+    private float turretRotationSmooth = 30f;
     private float lastAttackTime;
 
     private Transform player;
@@ -19,6 +21,7 @@ public class EnemyShot : MonoBehaviour
 
     private void Start()
     {
+
         // 始めにプレイヤーの位置を取得できるようにする
         player = GameObject.FindWithTag("Player").transform;
     }
@@ -28,13 +31,27 @@ public class EnemyShot : MonoBehaviour
         // 砲台をプレイヤーの方向に向ける
         Quaternion targetRotation = Quaternion.LookRotation(player.position - turret.position);
         turret.rotation = Quaternion.Slerp(turret.rotation, targetRotation, Time.deltaTime * turretRotationSmooth);
-        ray = new Ray(transform.position, muzzle.position);
+        ray = new Ray(new Vector3(muzzle.position.x, 1, muzzle.position.z)
+                        , new Vector3(player.position.x - muzzle.position.x
+                        , 1
+                        , player.position.z - muzzle.position.z));
+
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 0);
 
         // 一定間隔で弾丸を発射する
-        if (Time.time > lastAttackTime + attackInterval && Physics.Raycast(ray, out hit, 10f))
+        if (Physics.Raycast(ray, out hit, 20f)  && Time.time > lastAttackTime + attackInterval && hit.collider.tag == player.tag)
         {
+            ShotFlg = true;
+            Debug.Log(hit.collider.tag);
+        }
+        if (ShotFlg == true)
+        {
+            Debug.Log("hit");
             Instantiate(bulletPrefab, muzzle.position, muzzle.rotation);
             lastAttackTime = Time.time;
+
+            ShotFlg = false;
         }
+
     }
 }
