72deaee3d272c7aced5cb9a136fa42b2e7243ae6
moyo123 <yuki-t-17d@stu.kbc.ac.jp>
2019-11-22 13:08:12 +0900

    最新にアップデート

---
+++ TANK/Assets/YUKI/First/Script/EnemyShotVer2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShotVer2 : MonoBehaviour {
+
+    public Transform turret;
+    public Transform muzzle;
+    public GameObject bulletPrefab;
+
+    Ray ray;
+    RaycastHit hit;
+
+    private float attackInterval = 2f;
+    private float turretRotationSmooth = 30f;
+    private float lastAttackTime;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 砲台をプレイヤーの方向に向ける
+        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(Random.Range(-50,50),turret.position.y,turret.position.z) - turret.position);
+        turret.rotation = Quaternion.Slerp(turret.rotation, targetRotation, Time.deltaTime * turretRotationSmooth);
+
+
+        ray = new Ray(turret.position , muzzle.position - turret.position);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.green, 0);
+
+    }
+}
