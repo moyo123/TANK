553af217d0b296d574c56692d2247414d6da3c9c
kei-nakama <kei-n-17d@stu.kbc.ac.jp>
2020-01-24 11:24:17 +0900

    アセットの追加

---
+++ TANK/Assets/KEI/EffectManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour {
+    
+    private ParticleSystem particle;
+    
+    // Use this for initialization
+    //void Start ()
+    //{
+    //    particle = Resources.Load
+        
+    //    // ここで Particle System を停止する.
+    //    particle.Stop ();
+    //}
+    
+    //void OnTriggerEnter (Collider col)
+    //{
+    //    // ここで Particle System を開始します.
+    //    particle.Play ();
+    //}
+
+}
